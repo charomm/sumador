bb2819bd51e03ec04cfac0c2be9ba5c0861b844c
Charo Martínez <Maria@Charo>
2021-04-08 23:48:08 +0200

    Segunda prueba

--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -12,5 +12,12 @@ public class TestASumar {
         String resultado = sumi.sumativo("5");
         assertEquals("5 = 5",resultado );
     }
+	
+	@Test
+    public void testNegativo() {
+        ASumar sumi = new ASumar();
+        String resultado = sumi.sumativo("-5");
+        assertEquals("Negativo. Siempre negativo",resultado);
+    }
 
 }
