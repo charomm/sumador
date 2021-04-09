60145c4b2b23e611753a318c9fd59ed1c316302d
Charo Martínez <charomm@gmail.com>
2021-04-09 20:44:52 +0200

    Finalización

---
+++ sumador/src/sumador/MRMMFunciona.java
@@ -0,0 +1,13 @@
+package sumador;
+
+public class MRMMFunciona {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+        ASumar sumi = new ASumar();
+        String resultado = sumi.sumativo(args[0]);
+        System.out.println("Resultado " + resultado);
+		
+	}
+
+}
