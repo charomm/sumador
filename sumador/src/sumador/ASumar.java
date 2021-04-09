7dc2c66bb26bbc841a7ad53b99f11089dcc8ee92
Charo Martínez <Maria@Charo>
2021-04-09 19:00:07 +0200

    Revisión inicial

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,30 +1,5 @@
 package sumador;
 
-/**
- * @author Charo
- * @version: 1.0
- *
- */
 public class ASumar {
-
-	/**
-	 * Función que se le pasa una cadena formada por números y nos
-	 * devuelve dicha cadena sumando los digitos que la componen.
-	 * Ejemplo: "2654" no dará como resultado "2 + 6 + 5 + 4 = 17"
-	 * Si el número es negativo devolverá la cadena  "Negativo. Siempre negativo"
-	 * 
-	 * @param texto que contiene la cadena de números que hay que sumar: Ej: "2654"
-	 * @return cadena con el resultado de la suma. Ej: "2 + 6 + 5 + 4 = 17".
-	 */
-	public String sumativo(String numero) {
-        if (numero.length() == 1) {
-            return (numero + " = " + numero);
-        } else if (numero.length() > 1) {
-        	if (numero.substring(0,1).equals("-")) {
-        		return "Negativo. Siempre negativo";
-        	}
-        }
-        return "";
-	}
 
 }
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -7,17 +7,8 @@ import org.junit.Test;
 public class TestASumar {
 
 	@Test
-    public void testUnaCifra() {
-        ASumar sumi = new ASumar();
-        String resultado = sumi.sumativo("5");
-        assertEquals("5 = 5",resultado );
-    }
-	
-	@Test
-    public void testNegativo() {
-        ASumar sumi = new ASumar();
-        String resultado = sumi.sumativo("-5");
-        assertEquals("Negativo. Siempre negativo",resultado);
-    }
+	public void test() {
+		fail("No implementado aun");
+	}
 
 }
